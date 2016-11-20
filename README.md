2770ed8f5f3f205ce924486f0c810fde78eaa767
Paulo Costa <me@paulo.costa.nom.br>
2016-11-20 17:37:24 -0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# count-things
+# Count things
 Did you ever want a dead-simple service to assign build numbers, serial numbers, etc? This is it.
+
+## Counter Identifiers
+This service hosts many different counters -- How many builds have been made of v1.9.35 of libfoo, serial numbers of Device Foo, etc.
+
+To distinguish between those, each counter has an hierarchical Identifier, identified by it's path. for instance, `mycompany.com/libfoo/v1.9.35/build/` and `foodevices.com/foodevice/serial_number/`.
+
+Keep in mind that:
+- To avoid conflicts, the first element of the identifier should your domain name. For testing purposes, you should use `example.com`.
+- Counter identifiers always end with a trailling slash
+
+## Item identifier
+You can link an unique identifier to each value of the counter.
+
+For example, you can link your build number with the git commit hash, and your device serial number with it's MAC Address.
+
+If you use an identifier, the counter will be incremented the first time you access it, but the same value is always going to be returned afterwards.
---
+++ main.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from flask import Flask, request, redirect
+from google.appengine.ext import ndb
+
+
+app = Flask(__name__)
+
+class Namespace(ndb.Model):
+    namespace = ndb.StringProperty()
+    count = ndb.IntegerProperty()
+
+class Item(ndb.Model):
+    number = ndb.IntegerProperty()
+
+
+@app.route('/')
+def handler_root():
+    return redirect("https://github.com/paulo-raca/count-things")
+
+@app.route('/<path:namespaces>/')
+def handler_nokey(namespaces):
+    return get_count(namespaces.split('/'))
+
+@app.route('/<path:namespaces>/<key>')
+def handler_withkey(namespaces, key):
+    return get_count(namespaces.split('/'), key)
+
+def pretty_key(key):
+    return u'»'.join(key.flat()[1::2])
+
+@ndb.transactional
+def get_count(namespaces, item=None):
+    namespace_key=None
+    namespace_obj=None
+    
+    for namespace in namespaces:
+        namespace_key = ndb.Key(Namespace, namespace, parent=namespace_key)
+        namespace_obj = namespace_key.get()
+        if namespace_obj is None:
+            namespace_obj = Namespace(key=namespace_key, count=0)
+            namespace_obj.put()
+            logging.info('Created namespace %s', pretty_key(namespace_key))
+    
+    if item is None:
+        namespace_obj.count += 1
+        namespace_obj.put()
+        logging.info('Counting unnamed item on namespace %s = %d', pretty_key(namespace_key), namespace_obj.count)
+        return str(namespace_obj.count)
+    else:
+        item_key = ndb.Key(Item, item, parent=namespace_key)
+        item_obj = item_key.get()
+        if item_obj is None:
+            namespace_obj.count += 1
+            namespace_obj.put()
+            item_obj = Item(key=item_key, number=namespace_obj.count)
+            item_obj.put()
+            logging.info('Created item %s = %d', pretty_key(item_key), item_obj.number)
+        else:
+            logging.info('Existing item %s = %d', pretty_key(item_key), item_obj.number)
+        return str(item_obj.number)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
